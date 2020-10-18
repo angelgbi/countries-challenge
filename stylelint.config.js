3feeac3ee89fe9ce7b937884453dbe24bad6ddfa
Angel Briceño <abriceno@teravisiontech.com>
2020-10-18 15:37:56 -0400

    adding sass + config stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
+  extends: ['stylelint-config-sass-guidelines'],
+  plugins: ['stylelint-selector-bem-pattern', 'stylelint-scss'],
+  rules: {
+    'selector-class-pattern': null,
+    'at-rule-disallowed-list': null,
+    'plugin/selector-bem-pattern': {
+      componentName: '[A-Z]+',
+      componentSelectors: {
+        initial: '^\\.{componentName}(?:-[a-z]+)?$',
+        combined: '^\\.combined-{componentName}-[a-z]+$',
+      },
+      utilitySelectors: '^\\.util-[a-z]+$',
+    },
+    'selector-max-id': 2,
+    'max-nesting-depth': 3,
+    indentation: 2,
+  },
 }
