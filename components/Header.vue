469a103ac5c9d900d1c6a492e8831dca455950da
Angel Briceño <abriceno@teravisiontech.com>
2020-10-19 18:09:53 -0400

    Adding switch theme to header Applying theme to header

--- components/Header.vue
+++ components/Header.vue
@@ -1,21 +1,41 @@
 <template>
   <div class="header">
     <h1 class="header__title">Where in the world?</h1>
-    <div class="header__theme-switch">
+    <div
+      v-if="isDark"
+      class="header__theme-switch"
+      @click="switchTheme('light')"
+    >
       <span class="fa fa-moon-o"></span> Dark Mode
+    </div>
+    <div v-else class="header__theme-switch" @click="switchTheme('dark')">
+      <span class="fa fa-moon-o"></span> Light Mode
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      isDark: false,
+    }
+  },
+  methods: {
+    switchTheme(theme) {
+      this.$nuxt.$colorMode.preference = theme
+      this.isDark = !this.isDark
+    },
+  },
+}
 </script>
 
 <style lang="scss">
 .header {
   @apply flex flex-row justify-between items-center;
   @apply py-8 px-4;
-  @apply shadow-md;
+  @apply shadow-md z-10 relative;
+  background-color: var(--bg-secondary);
 
   &__title {
     @apply text-base font-extrabold;
@@ -35,7 +55,7 @@ export default {}
     }
 
     &__theme-switch {
-      @apply text-base;
+      @apply text-base cursor-pointer;
     }
   }
 }
